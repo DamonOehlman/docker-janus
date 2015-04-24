eb6b071c200949e9a448b4431d92f134ac3253c9
Damon Oehlman <damon.oehlman@gmail.com>
2015-04-24 17:59:34 +1000

    write a config.site file for files writing to $DEPS_HOME

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,3 +2,9 @@
 
 # dependency locations (no sudo required)
 mkdir -p $DEPS_HOME/dl
+mkdir -p $DEPS_HOME/share
+
+cat << EOF > $DEPS_HOME/share/config.site
+CPPFLAGS=-I$DEPS_HOME/include
+LDFLAGS=-L$DEPS_HOME/lib
+EOF
